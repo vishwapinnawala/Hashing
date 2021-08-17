863a68cd5067ec10fb751749d8f3b2c0462e123b
Vishwa Pinnawala <45973257+vishwapinnawala@users.noreply.github.com>
2021-08-17 18:08:12 +0530

    Add files via upload

---
+++ Encryption/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Encryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.showbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(80, 31);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(151, 20);
+            this.input.TabIndex = 1;
+            // 
+            // input2
+            // 
+            this.input2.Location = new System.Drawing.Point(80, 67);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(151, 20);
+            this.input2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Store";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "UserID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 179);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // showbtn
+            // 
+            this.showbtn.Location = new System.Drawing.Point(23, 150);
+            this.showbtn.Name = "showbtn";
+            this.showbtn.Size = new System.Drawing.Size(75, 23);
+            this.showbtn.TabIndex = 7;
+            this.showbtn.Text = "Show";
+            this.showbtn.UseVisualStyleBackColor = true;
+            this.showbtn.Click += new System.EventHandler(this.showbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 352);
+            this.Controls.Add(this.showbtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button showbtn;
+    }
+}
+
---
+++ Encryption/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;//to md5 hash use this 
+using System.Data.SqlClient;//to use sql commands use this
+using System.Text.RegularExpressions;//to user regex input this
+using System.IO;//to use get the file path use this
+
+
+namespace Encryption
+{
+    public partial class Form1 : Form
+    {
+        //////////////////////////////////////
+        //https://www.youtube.com/watch?v=da5GSeJV9qE - hashing, refer this video
+        string newpwd;
+        static string path = Path.GetFullPath(Environment.CurrentDirectory);
+        static string databasename = "pwddb.mdf";
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=" + path + @"\" + databasename + ";Integrated Security=True");
+        //SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\USERNAME\Documents\Visual Studio 2013\Projects\C#\Encryption\Encryption\pwddb.mdf;Integrated Security=True");
+        /////////////////////////////////////
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string Encrypt(string value)//encrypting function
+        {
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                UTF8Encoding utf8 = new UTF8Encoding();
+                byte[] data = md5.ComputeHash(utf8.GetBytes(value));
+                return Convert.ToBase64String(data);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string encpwd = Encrypt(input2.Text);//hashing input2 text
+               
+
+                string searchqry = "Select * from login where userid= " + input.Text + " ";
+                SqlCommand cmd = new SqlCommand(searchqry, con);
+                con.Open();
+                SqlDataReader r = cmd.ExecuteReader();
+                
+
+                while (r.Read())
+                {
+                 newpwd=r[1].ToString();
+                }
+
+              
+                /*without this the data type in table ex: varchar(100) say your pwd is vishwa 
+                its 6 char right the rest of 94 space you left will write spaces.
+                so here we are removing those spaces
+                with spaces if statement won't work*/
+
+                newpwd = Regex.Replace(newpwd, @"\s", "");
+                
+
+                if (encpwd == newpwd)
+                {
+                    //input2.Text = newpwd;
+                    MessageBox.Show("Login Successfull!");
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed!");
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error Occured" + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+   
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string encpwd = Encrypt(input2.Text);//hashing the input2 text
+
+            string qry = "INSERT INTO login VALUES(" + input.Text + ",'" + encpwd + "')";
+            SqlCommand cmd = new SqlCommand(qry, con);
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Inserted Successfully!");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error Occured" + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+
+        }
+
+        private void showbtn_Click(object sender, EventArgs e)
+        {
+            string qry = "SELECT * FROM login";
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(qry,con);
+                DataSet ds = new DataSet();
+                da.Fill(ds, "login");
+                dataGridView1.DataSource = ds.Tables["login"];
+            }
+            catch(SqlException Ex)
+            {
+                MessageBox.Show("Error Occured" + Ex.ToString());
+            }
+        }
+
+      
+    }
+}
